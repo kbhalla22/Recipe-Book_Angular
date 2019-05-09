7d22c0fcea4a4e3b81f0057b9f8da245bb616fbe
karanbhalla <kbhalla22@gmail.com>
2019-05-09 12:17:23 -0400

    Updated recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[]=[new Recipe('A test Recipe','This is simply a test','https://www.google.com/imgres?imgurl=https%3A%2F%2F144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com%2Fuploads%2Ffood_portal_data%2Frecipes%2Frecipe%2Fhero_article_image%2F4126%2Fe890ffc91a85684b63ff%2Fletterbox_ButterChicken.jpg&imgrefurl=https%3A%2F%2Frecipes.sainsburys.co.uk%2Frecipes%2Fmain-courses%2Fhealthier-butter-chicken&docid=uyaDAJdSHr-wyM&tbnid=E4Phlwtg8ZwJ6M%3A&vet=10ahUKEwiryoXR_IziAhWrl-AKHSzRDfMQMwiaASgjMCM..i&w=772&h=434&client=safari&bih=748&biw=1348&q=recipes&ved=0ahUKEwiryoXR_IziAhWrl-AKHSzRDfMQMwiaASgjMCM&iact=mrc&uact=8')];
+  recipes: Recipe[]=[new Recipe('A test Recipe','This is simply a test','https://cdn.pixabay.com/photo/2017/06/01/18/46/cook-2364221_1280.jpg')];
 
   constructor() { }
 
